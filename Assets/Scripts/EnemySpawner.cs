f7f36ea49c18a57f3e2bb8270f5176b6f6ee8502
OndAnde <pashamanulik@gmail.com>
2022-07-11 16:21:39 +0300

    local lesson 2 chng

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,9 +6,11 @@ public class EnemySpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject enemy;
+    [SerializeField] private int enemyValue;
     [SerializeField] private float spawnStep = 1f;
 
     private float timeToSpawn;
+    private int currentEnemyValue = 0;
     
 
     // Start is called before the first frame update
@@ -20,11 +22,11 @@ public class EnemySpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     if (Time.time > timeToSpawn)
+     if (Time.time > timeToSpawn && currentEnemyValue < enemyValue)
         {
-            
             Instantiate(enemy, transform);
             timeToSpawn = Time.time + spawnStep;
+            currentEnemyValue++;
         }   
     }
 
